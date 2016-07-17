1e54596dc29b20efa1d5ab9904ff9d4ec53ce922
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2016-07-17 21:54:30 +0200

    Split CLI from class suite

--- broadband_psf.py
+++ broadband_psf.py
@@ -11,9 +11,6 @@ from __future__ import print_function, division
 
 import os
 import copy
-import time
-import argparse
-import configparser
 import astropy.units as u
 import astropy.io.fits as pyfits
 import numpy as np
@@ -653,158 +650,14 @@ def local_test():
     transfile = os.path.join(dpath, 'broadband/throughput/VISThroughBE.dat')
     outfile = os.path.join(dpath, 'psf_broadband_test.fits')
 
-        Parameters
-        ----------
-        outfits: str
-            Output FITS file
-        clobber: bool, optional
-            Overwrite file if already existing (default `False`)
-
-        """
-        self.update_header_info()
-
-        header = copy.copy(self.header)
-        comment = ("========================================="
-                   "Broadband PSF created from {} wavelengths"
-                   "=========================================")
-        header.add_comment(comment.format(len(self.wavelength)))
-        try:
-            pyfits.writeto(outfits, data=self.psf,
-                           header=header, clobber=clobber)
-            print("Output image written in {}".format(outfits))
-        except:
-            print("Output file {} already existing".format(outfits))
-
-    def update_header_info(self):
-        """
-        Ensure the header has the minimum required entries
-        """
-        if self.header is None:
-            hdu = pyfits.PrimaryHDU(data=self.psf)
-            self.header = hdu.header
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('psf_cube_files', type=str, nargs='+',
-                        help='Monochromatic PSF FITS files')
-    parser.add_argument('sed', type=str, nargs='+',
-                        help='Source energy distribution of stars')
-    parser.add_argument('-c', '--config', type=str, default=None,
-                        help="Configuration file")
-
-    return parser.parse_args()
-
-
-def extract_config(configfile):
-    """
-    Extract typed information from config parser
-
-    Parameters
-    ----------
-    configfile: str
-        Path to configuration text file
-
-    Returns
-    -------
-    dictionary of useful parameters
-
-    """
-    confparams = configparser.SafeConfigParser()
-    confparams.read(configfile)
-
-    # PSF
-    n_wl = confparams.getint('psf', 'n_wavelength')
-    psf_size = confparams.getint('psf', 'size')
-    cube_shape = (n_wl, psf_size, psf_size)
-    pixel_size = confparams.getfloat('psf', 'pixel_size')
-    psf_wlunit = getattr(u, confparams.get('psf', 'wlunit').lower())
-
-    psf_dict = dict(shape=cube_shape, pixel_size=pixel_size, wlunit=psf_wlunit)
-
-    # Telescope
-    thruput_filename = confparams.get('thruput', 'filename')
-    thruput_cols = [confparams.getint('thruput', 'wl_col'),
-                    confparams.getint('thruput', 'thruput_col')]
-    thruput_wlunit = getattr(u, confparams.get('thruput', 'wlunit').lower())
-
-    thruput_dict = dict(filename=thruput_filename, cols=thruput_cols,
-                        wlunit=thruput_wlunit)
-
-    # SED
-    sed_type = confparams.get('sed', 'type')
-    sed_wlunit = getattr(confparams.get('sed', 'wlunit').lower())
-
-    sed_dict = dict(wlunit=sed_wlunit)
-
-    # Broadband
-    wl_step = confparams.getfloat('broadband', 'wl_step')
-    down_fact = confparams.getfloat('broadband', 'down_fact')
-
-    outputfile = os.path.join(confparams.get('output', 'path'),
-                              confparams.get('output', 'nameformat'))
-
-    return dict(psf=psf_dict,
-                thruput=thruput_dict,
-                sed=sed_dict,
-                sed_type=sed_type,
-                wl_step=wl_step,
-                down_fact=down_fact,
-                outputfile=outputfile)
-
-
-def main():
-    args = parse_args()
-
-    conf = extract_config(args.config)
-
-    for psf_file in args.psf_cube_files:
-        psf = PSFcube.from_fits(psf_file, **conf['psf'])
-        thruput = TelescopeThroughput.from_file(**conf['thruput'])
-        for sed_file in args.sed_files:
-            if conf['sed_type'].lower() == 'fits':
-                sed = SED.from_fits(sed_file, **conf['sed'])
-            else:
-                sed = SED.from_ascii(sed_file, **conf['sed'])
-
-            psf.to_broadband(sed=sed, transmission=thruput,
-                             wl_step=conf['wl_step'])
-
-            psf_basename = os.path.basename(os.path.splitext(psf_file)[0])
-            sed_basename = os.path.basename(os.path.splitext(sed_file)[0])
-            outfile = conf['outputfile'].format(psf=psf_basename,
-                                                sed=sed_basename)
-            psf.write_effective_psf(outfile)
-
-            if conf['down_fact'] > 1:
-                psf.downsample_detector(conf['down_fact'])
-                psf.write_effective_psf(outfile.replace('.fits',
-                                                        '_downsampled.fits'))
-
-
-def test():
-    DPATH = '/Users/aboucaud/work/IAS/stage/tperdereau/'
-    BPZ = "/Users/aboucaud/work/Euclid/data/galaxy_spectra/bpz/{0}_B2004a.sed"
-
-    fitsfile = os.path.join(DPATH, 'psf_test_euclid.fits')
-    sedtype = 'irr'
-    transfile = os.path.join(DPATH, 'broadband/throughput/VISThroughBE.dat')
-    outfile = os.path.join(DPATH, 'psf_broadband_test.fits')
-
-    start_time = time.time()
-
-    P = PSFcube.from_fits(fitsfile, shape=(11, 512, 512), pixel_size=2, wlunit=u.um)
-    S = FlatSED()
-    # S = SED.from_ascii(BPZ.format(sedtype))
-    T = TelescopeThroughput.from_file(transfile, cols=[0, 4], wlunit=u.nm)
-    P.to_broadband(sed=S, transmission=T, wl_step=10)
-    P.write_effective_psf(outfile)
-    P.downsample_detector(4)
-    P.write_effective_psf(outfile.replace('.fits', '_dwspl.fits'))
-
-    dtime = int(time.time() - start_time)
-    time_text = "Elapsed time: {0:02d}h{1:02d}m{2:02d}s"
-    print(time_text.format(dtime // 3600, (dtime % 3600) // 60, dtime % 60))
+    psf = PSFcube.from_fits(fitsfile, shape=(11, 512, 512),
+                            pixel_size=2, wlunit=u.um)
+    sed = FlatSED()
+    thru = TelescopeThroughput.from_file(transfile, cols=[0, 4], wlunit=u.nm)
+    psf.to_broadband(sed=sed, thruput=thru, wl_step=10)
+    psf.write_effective_psf(outfile)
+    psf.downsample_detector(4)
+    psf.write_effective_psf(outfile.replace('.fits', '_dwspl.fits'))
 
 
 if __name__ == '__main__':
---
+++ mk_broadband.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+mk_broadband_psf
+----------------
+Create a series of broadband PSFs given input various SED and monochromatic
+PSF files as well as an instrumental thruput
+
+"""
+from __future__ import print_function, division
+
+import os
+import time
+import argparse
+import configparser
+import astropy.units as u
+import broadband_psf as bp
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('psf_cube_files', type=str, nargs='+',
+                        help='Monochromatic PSF FITS files')
+    parser.add_argument('sed_files', type=str, nargs='+',
+                        help='Source energy distribution of stars')
+    parser.add_argument('-c', '--config', type=str, default=None,
+                        help="Configuration file")
+
+    return parser.parse_args()
+
+
+def extract_config(configfile):
+    """
+    Extract typed information from config parser or return default
+
+    Parameters
+    ----------
+    configfile: str
+        Path to configuration text file
+
+    Returns
+    -------
+    dictionary of useful parameters
+
+    """
+    confparams = configparser.SafeConfigParser()
+    confparams.read(configfile)
+
+    # PSF
+    n_wl = confparams.getint('psf', 'n_wavelength')
+    psf_size = confparams.getint('psf', 'size')
+    cube_shape = (n_wl, psf_size, psf_size)
+    pixel_size = confparams.getfloat('psf', 'pixel_size')
+    psf_wlunit = getattr(u, confparams.get('psf', 'wlunit').lower())
+
+    psf_dict = dict(shape=cube_shape, pixel_size=pixel_size, wlunit=psf_wlunit)
+
+    # Telescope
+    thruput_filename = confparams.get('thruput', 'filename')
+    thruput_cols = [confparams.getint('thruput', 'wl_col'),
+                    confparams.getint('thruput', 'thruput_col')]
+    thruput_wlunit = getattr(u, confparams.get('thruput', 'wlunit').lower())
+
+    thruput_dict = dict(filename=thruput_filename, cols=thruput_cols,
+                        wlunit=thruput_wlunit)
+
+    # SED
+    sed_type = confparams.get('sed', 'type')
+    sed_wlunit = getattr(u, confparams.get('sed', 'wlunit').lower())
+
+    sed_dict = dict(wlunit=sed_wlunit)
+
+    # Broadband
+    wl_step = confparams.getfloat('broadband', 'wl_step')
+    niemi = confparams.getboolean('broadband', 'use_niemi')
+    aocs = confparams.getboolean('broadband', 'use_aocs')
+    prf = confparams.getboolean('broadband', 'use_prf')
+    down_fact = confparams.getfloat('broadband', 'down_factor')
+
+    broadband_dict = dict(wl_step=wl_step, niemi=niemi, aocs=aocs, prf=prf)
+
+    outputfile = os.path.join(confparams.get('output', 'path'),
+                              confparams.get('output', 'nameformat'))
+
+    return dict(psf=psf_dict,
+                thruput=thruput_dict,
+                sed=sed_dict,
+                sed_type=sed_type,
+                broadband=broadband_dict,
+                down_fact=down_fact,
+                outputfile=outputfile)
+
+
+def main():
+    args = parse_args()
+
+    conf = extract_config(args.config)
+
+    start_time = time.time()
+
+    for psf_file in args.psf_cube_files:
+        psf = bp.PSFcube.from_fits(psf_file, **conf['psf'])
+        thruput = bp.TelescopeThroughput.from_file(**conf['thruput'])
+        for sed_file in args.sed_files:
+            if conf['sed_type'].lower() == 'fits':
+                sed = bp.SED.from_fits(sed_file, **conf['sed'])
+            else:
+                sed = bp.SED.from_ascii(sed_file, **conf['sed'])
+
+            psf.to_broadband(sed=sed, thruput=thruput, **conf['broadband'])
+
+            psf_basename = os.path.basename(os.path.splitext(psf_file)[0])
+            sed_basename = os.path.basename(os.path.splitext(sed_file)[0])
+            outfile = conf['outputfile'].format(psf=psf_basename,
+                                                sed=sed_basename)
+            psf.write_effective_psf(outfile)
+
+            if conf['down_fact'] > 1:
+                psf.downsample_detector(conf['down_fact'])
+                psf.write_effective_psf(outfile.replace('.fits',
+                                                        '_downsampled.fits'))
+
+    dtime = int(time.time() - start_time)
+    time_text = ("\n---\n"
+                 "Elapsed time: {0:02d}h{1:02d}m{2:02d}s")
+    print(time_text.format(dtime // 3600, (dtime % 3600) // 60, dtime % 60))
+
+
+if __name__ == '__main__':
+    main()

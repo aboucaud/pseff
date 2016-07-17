82a5502af9837f045c72c3db41387117f5bd482b
Alexandre Boucaud <boucaud.alexandre@gmail.com>
2016-07-17 21:53:37 +0200

    Restructure class methods and add docstrings

--- broadband_psf.py
+++ broadband_psf.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Broadband PSF
+=============
+Convert a series of monochromatic PSF images into an instrumental
+broadband PSF given a stellar spectral energy distribution
+
+"""
 from __future__ import print_function, division
 
 import os
@@ -253,6 +260,178 @@ class PSFcube(object):
 
         return cls(wavelength, data, pixel_size, header=hdr, wlunit=wlunit)
 
+    @property
+    def psf(self):
+        """Accessor for the broadband PSF"""
+        if not self._psf_computed:
+            print("Broadband PSF not computed yet")
+            return None
+
+        return self._psf
+
+    def expand_psf_cube(self):
+        """
+        Interpolate the wavefront pixels to expand the data along lambda
+        """
+        rtf_cube = npf.rfftn(npf.ifftshift(self.psf_cube), axes=(-2, -1))
+
+        _, psf_size, rtf_size = rtf_cube.shape
+
+        expanded_rtf_cube = np.empty((self.expanded_range.size,
+                                      psf_size, rtf_size),
+                                     dtype=complex)
+
+        for x in range(rtf_size):
+            for y in range(psf_size):
+                # Extract the real and imaginary functions at the current point
+                expanded_pixel = self.expand_carray(rtf_cube[:, y, x],
+                                                    self.wavelength,
+                                                    self.expanded_range)
+                # Store the expanded (x, y) pixel point acorss the full cube.
+                expanded_rtf_cube[:, y, x] = expanded_pixel
+
+        return expanded_rtf_cube
+
+    def to_broadband(self, sed, thruput, wl_step=1,
+                     niemi=False, aocs=False, prf=False):
+        """
+        Compute the broadband PSF from the cube of monochromatic PSFs
+
+        Parameters
+        ----------
+        sed: `SED`
+            Instance of a stellar SED
+        thruput: `TelescopeThroughput`
+            Instance of a telescope throughput
+        wl_step: float or int, optional
+            Linear step between wavelength in expanded range
+            (default is 1)
+        niemi: bool, optional
+            Apply the Niemi effect (default `False`)
+        aocs: bool, optional
+            Apply the AOCS effect (default `False`)
+        prf: bool, optional
+            Apply the  pixel response effect (default `False`)
+
+        Returns
+        -------
+        broadband_psf: array_like
+            Image of the broadband PSF
+
+        """
+        # Create the range of wavelengths for interpolation
+        self.expanded_range = np.arange(self.wavelength[0],
+                                        self.wavelength[-1] + 1,
+                                        step=wl_step)
+
+        # Retrieve the expanded wavefront
+        expanded_wf = self.expand_psf_cube()
+
+        # Apply SED + Telescope throughput weighting
+        total_thruput = (sed(self.expanded_range, ext_wlunit=self.wlunit) *
+                         thruput(self.expanded_range, ext_wlunit=self.wlunit))
+        expanded_wf *= total_thruput[:, None, None]
+
+        # Apply the Niemi Effect (WL Dependant)
+        if niemi:
+            expanded_wf *= self.niemi_effect(expanded_wf.shape,
+                                             self.expanded_range,
+                                             self.pixel_size)
+
+        # Integrate of wavelength range
+        broadband_wf = np.sum(expanded_wf, axis=0)
+
+        # Apply AOCS Effect to the coadded PSF (Not WL Dependant)
+        if aocs:
+            broadband_wf *= self.aocs_effect()
+
+        # Apply SubPixel Response Effect to the coadded PSF (Not WL Dependant)
+        if prf:
+            broadband_wf *= self.pixelresponse_effect()
+
+        broadband_psf = np.abs(npf.ifftshift(npf.irfft2(broadband_wf)))
+
+        self._psf = self.normalize(broadband_psf)
+        self._psf_computed = True
+
+    def downsample_detector(self, ds_factor=1):
+        """
+        Downsampled the broadband PSF array to detector resolution.
+
+        Parameters
+        ----------
+        ds_factor: float, optional
+            Downsampling factor to apply to the broadband PSF
+            (default is 1)
+
+        """
+        if ds_factor > 1:
+            psf_shape = self.psf.shape
+
+            y, x = np.indices(psf_shape) // ds_factor
+            new_shape = np.array(psf_shape, dtype=int) // ds_factor
+
+            regions = x + y * psf_shape[1] // ds_factor
+
+            psf_sum = spn.sum(self.psf,
+                              labels=regions,
+                              index=np.arange(regions.max() + 1))
+            downsampled_psf = psf_sum.reshape(*new_shape)
+
+            self._psf = self.normalize(downsampled_psf)
+
+    def write_effective_psf(self, outfits, clobber=False):
+        """
+        Write broadband PSF to disk
+
+        Parameters
+        ----------
+        outfits: str
+            Output FITS file
+        clobber: bool, optional
+            Overwrite file if already existing (default `False`)
+
+        """
+        self._update_header_info()
+
+        header = copy.copy(self.header)
+        comment = ("========================================="
+                   "Broadband PSF created from {} wavelengths"
+                   "=========================================")
+        header.add_comment(comment.format(len(self.wavelength)))
+        try:
+            pyfits.writeto(outfits, data=self.psf,
+                           header=header, clobber=clobber)
+            print("Output image written in {}".format(outfits))
+        except:
+            print("Output file {} already existing".format(outfits))
+
+    def _update_header_info(self):
+        """
+        Ensure the header has the minimum required entries
+        """
+        if self.header is None:
+            hdu = pyfits.PrimaryHDU(data=self.psf)
+            self.header = hdu.header
+
+    @staticmethod
+    def normalize(data):
+        """
+        Normalize to unity
+
+        Parameters
+        ----------
+        data: array_like
+            Array data to normalize
+
+        Returns
+        -------
+        normalized_data: array_like
+            Normalized array to unity
+
+        """
+        return data / data.sum()
+
     @staticmethod
     def niemi_effect(data_shape, wavelength, pixel_size,
                      niemi_params=(17.45, -0.237, 45.10, -0.378)):
@@ -467,183 +646,12 @@ class PSFcube(object):
 
         return interpolated_data
 
-    def expand_psf_cube(self):
-        """
-        Interpolate the wavefront pixels to expand the data along lambda
-        """
-        rtf_cube = npf.rfftn(npf.ifftshift(self.psf_cube), axes=(-2, -1))
 
-        _, psf_size, rtf_size = rtf_cube.shape
-
-        expanded_rtf_cube = np.empty((self.expanded_range.size,
-                                      psf_size, rtf_size),
-                                     dtype=complex)
-
-        for x in range(rtf_size):
-            for y in range(psf_size):
-                # Extract the real and imaginary functions at the current point.
-                expanded_pixel = self.expand_carray(rtf_cube[:, y, x],
-                                                    self.wavelength,
-                                                    self.expanded_range)
-                # Store the expanded (x, y) pixel point acorss the full cube.
-                expanded_rtf_cube[:, y, x] = expanded_pixel
-
-        return expanded_rtf_cube
-
-    def to_broadband(self, sed, transmission, wl_step=1,
-                     niemi=False, aocs=False, prf=False):
-        """
-        Compute the broadband PSF from the cube of monochromatic PSFs
-
-        Parameters
-        ----------
-        sed: `SED`
-            Instance of a stellar SED
-        transmission: `TelescopeThroughput`
-            Instance of a telescope throughput
-        wl_step: float or int, optional
-            Linear step between wavelength in expanded range
-            (default is 1)
-        niemi: bool, optional
-            Apply the Niemi effect (default `False`)
-        aocs: bool, optional
-            Apply the AOCS effect (default `False`)
-        prf: bool, optional
-            Apply the  pixel response effect (default `False`)
-
-        Returns
-        -------
-        broadband_psf: array_like
-            Image of the broadband PSF
-
-        """
-        # Create the range of wavelengths for interpolation
-        self.expanded_range = np.arange(self.wavelength[0],
-                                        self.wavelength[-1] + 1,
-                                        step=wl_step)
-
-        # Retrieve the expanded wavefront
-        expanded_wf = self.expand_psf_cube()
-
-        # Apply SED + Telescope throughput weighting
-        total_thruput = (sed(self.expanded_range, wlunit=self.wlunit) *
-                         transmission(self.expanded_range, wlunit=self.wlunit))
-        expanded_wf *= total_thruput[:, None, None]
-
-        # Apply the Niemi Effect (WL Dependant)
-        if niemi:
-            expanded_wf *= self.niemi_effect(expanded_wf.shape,
-                                             self.expanded_range,
-                                             self.pixel_size)
-
-        # Integrate of wavelength range
-        broadband_wf = np.sum(expanded_wf, axis=0)
-
-        # Apply AOCS Effect to the coadded PSF (Not WL Dependant)
-        if aocs:
-            broadband_wf *= self.aocs_effect()
-
-        # Apply SubPixel Response Effect to the coadded PSF (Not WL Dependant)
-        if prf:
-            broadband_wf *= self.pixelresponse_effect()
-
-        broadband_psf = np.abs(npf.ifftshift(npf.irfft2(broadband_wf)))
-
-        self._psf = self.normalize(broadband_psf)
-        self._psf_computed = True
-
-    # def run(self, save=False):
-    #     if not self._initialized_templates:
-    #         self.load_templates()
-
-    #     self.init_normalization_factor()
-
-    #     counter = "Computing pixel {0}/{1} ({2:d}%)"
-    #     print("\n")
-
-    #     _, nrows, ncols = self.psf_cube.shape
-    #     ntot = nrows * ncols
-    #     self._psf = np.zeros((nrows, ncols), dtype=float)
-    #     start_time = time.time()
-    #     for irow in range(nrows):
-    #         for icol in range(ncols):
-    #             print('\r', end='')
-    #             i_pix = irow*ncols + icol + 1
-    #             sys.stdout.write(counter.format(i_pix, ntot,
-    #                                             int(i_pix*100 / ntot)))
-    #             sys.stdout.flush()
-
-    #             self._psf[irow, icol] = self.poor_effective_pixel_response(irow, icol)
-
-    #     dtime = int(time.time() - start_time)
-    #     print("\nElapsed time: {0:02d}h{1:02d}m{2:02d}s\n".format(
-    #         dtime // 3600, (dtime % 3600) // 60 , dtime % 60))
-    #     print("Euclid effective PSF computed for",
-    #           "file {}".format(self.filename),
-    #           "and source {}".format(self.source),
-    #           sep="\n\t")
-
-    #     self._initialized_computation = True
-
-    #     if save:
-    #         self.write_effective_psf(save, self.header)
-
-    def downsample_detector(self, ds_factor=1):
-        """
-        Downsampled the broadband PSF array to detector resolution.
-
-        Parameters
-        ----------
-        ds_factor: float, optional
-            Downsampling factor to apply to the broadband PSF
-            (default is 1)
-
-        """
-        if ds_factor > 1:
-            psf_shape = self.psf.shape
-
-            y, x = np.indices(psf_shape) // ds_factor
-            new_shape = tuple(np.array(psf_shape, dtype=int) // ds_factor)
-
-            regions = x + y * psf_shape[1] // ds_factor
-
-            psf_sum = spn.sum(self.psf,
-                              labels=regions,
-                              index=np.arange(regions.max() + 1))
-            downsampled_psf = psf_sum.reshape(new_shape)
-
-            self._psf = self.normalize(downsampled_psf)
-
-    @property
-    def psf(self):
-        """Accessor for the broadband PSF"""
-        if not self._psf_computed:
-            print("Broadband PSF not computed yet")
-            return None
-
-        return self._psf
-
-    @staticmethod
-    def normalize(data):
-        """
-        Normalize to unity
-
-        Parameters
-        ----------
-        data: array_like
-            Array data to normalize
-
-        Returns
-        -------
-        normalized_data: array_like
-            Normalized array to unity
-
-        """
-        return data / data.sum()
-
-    def write_effective_psf(self, outfits, clobber=False):
-        """
-        Write broadband PSF to disk
+def local_test():
+    dpath = '/Users/aboucaud/work/IAS/stage/tperdereau/'
+    fitsfile = os.path.join(dpath, 'psf_test_euclid.fits')
+    transfile = os.path.join(dpath, 'broadband/throughput/VISThroughBE.dat')
+    outfile = os.path.join(dpath, 'psf_broadband_test.fits')
 
         Parameters
         ----------
@@ -800,7 +808,4 @@ def test():
 
 
 if __name__ == '__main__':
-    main()
-
-
-# work on the header
+    local_test()
